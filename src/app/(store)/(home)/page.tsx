7d7d3e5d383793b9ba7ea7d93ac3e7cd79585444
Raiane <raiane.oliveira404@gmail.com>
2023-11-02 12:48:10 -0300

    feat: style of home page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,3 +1,80 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <main className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+      <Link
+        href="/"
+        className="relative group col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-start"
+      >
+        <Image
+          className="group-hover:scale-105 transition-transform duration-500"
+          src="/moletom-never-stop-learning.png"
+          width={920}
+          height={920}
+          alt=""
+          quality={100}
+        />
+
+        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[288px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span
+            title="Moletom Never Stop Learning"
+            className="text-sm truncate"
+          >
+            Moletom Never Stop Learning
+          </span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="relative group col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-start"
+      >
+        <Image
+          className="group-hover:scale-105 transition-transform duration-500"
+          src="/moletom-ai-side.png"
+          width={920}
+          height={920}
+          alt=""
+          quality={100}
+        />
+
+        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[288px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span title="Moletom AI Side" className="text-sm truncate">
+            Moletom AI Side
+          </span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="relative group col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-start"
+      >
+        <Image
+          className="group-hover:scale-105 transition-transform duration-500"
+          src="/camiseta-dowhile-2022.png"
+          width={920}
+          height={920}
+          alt=""
+          quality={100}
+        />
+
+        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[288px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span title="Camiseta DoWhile 2022" className="text-sm truncate">
+            Camiseta DoWhile 2022
+          </span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129
+          </span>
+        </div>
+      </Link>
+    </main>
+  )
 }
