b1760e8c57fb3842e4e093b9e4e7d826e0739d78
Raiane <raiane.oliveira404@gmail.com>
2023-11-07 11:42:29 -0300

    feat: cart context and open-graph image generation

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,11 +1,14 @@
 import { Header } from '@/components/header'
+import { CartProvider } from '@/contexts/cart-context'
 import { ReactNode } from 'react'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] gap-5 p-8 grid-rows-[min-content_max-content]">
-      <Header />
-      {children}
-    </div>
+    <CartProvider>
+      <div className="mx-auto grid min-h-screen w-full max-w-[1600px] gap-5 p-8 grid-rows-[min-content_max-content]">
+        <Header />
+        {children}
+      </div>
+    </CartProvider>
   )
 }
